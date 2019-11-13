6bc2f39a1bccc4a141c686f97517be98453a0c54
DG <donavingrouette@live.com>
2019-11-12 22:08:45 -0600

    Made Adjustments to Main Styles

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,4 @@
 class List < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 5, maximum: 100}
+  validates :title, presence: true, length: {minimum: 5, maximum: 75}
   has_many :todos, dependent: :destroy
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 5, maximum: 100}
+  validates :title, presence: true, length: {minimum: 5, maximum: 75}
   validates :description, presence: true, length: {minimum: 5, maximum: 255}
   belongs_to :list, optional: true
 end
