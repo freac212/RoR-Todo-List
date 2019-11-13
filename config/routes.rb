0a4ebe9d3128b8e4439da13d3d6f5bd05d773690
DG <donavingrouette@live.com>
2019-11-12 20:48:59 -0600

    Added Basic Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lists/index'
+  root 'lists#index'
+
+  resources :lists, only: [:index, :new, :create, :destroy] do
+    resources :todos, only: [:index, :new, :create, :destroy]
+  end
+
+  resources :todos, only: [:index, :destroy]
 end
